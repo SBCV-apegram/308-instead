baf3bac008811c436cd1c978eacc8194f3c76462
Andrew Pegram <89920666+SBCV-apegram@users.noreply.github.com>
2025-12-16 14:50:56 -0500

    Update plugin.php
    Change 302 to 308, update header information to my repo

--- plugin.php
+++ plugin.php
@@ -1,20 +1,20 @@
 <?php
   /*
-Plugin Name: 302 Instead
-Plugin URI: https://github.com/EpicPilgrim/302-instead
-Description: Send a 302 (temporary) redirect instead of 301 (permanent) for sites where shortlinks may change
-Version: 1.3
-Author: BrettR
-Author URI: https://github.com/EpicPilgrim/302-instead
+Plugin Name: 308 Instead
+Plugin URI: https://github.com/SBCV-apegram/308-instead
+Description: Send a 308 (permanent) redirect instead of 301 (permanent) to preserve the HTTP method of the original request
+Version: 1.0
+Author: SBCV-apegram
+Author URI: https://github.com/SBCV-apegram/308-instead
   */
 
 yourls_add_action('pre_redirect','temp_instead_function');
 function temp_instead_function($args) {
   $url  = $args[0];
   $code = $args[1];
-  if ($code != 302) {
+  if ($code != 308) {
     // Redirect with 302 instead
-    yourls_redirect($url,302);
+    yourls_redirect($url,308);
     die();
   }
 }
